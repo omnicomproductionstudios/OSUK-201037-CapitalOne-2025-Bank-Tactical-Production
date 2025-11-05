ec28e662116a8b2c6a3291a57615b4497a593ba1
Mahesh Bhatt <141926871+adamandeveddbstudios@users.noreply.github.com>
2025-11-05 16:09:22 +0530

    new route completed

---
+++ RM_No_Fees/Bank_HOLIDAY_COBG_No_Fees_NA_RM_RM_EN_NA_1280x100_P00114026/manifest.js
@@ -0,0 +1,14 @@
+FT.manifest({
+	"filename":"Bank_Cafe_Spot_Check_NA_RM_RM_EN_NA_1280x100_P00102777.html",
+	"width":1280,
+	"height":100,
+	"clickTagCount":1,
+  "videos":[{"name":"video1", "ref":"202681/BANK_CAFE_SPOT_CHECK_NA_VID_VID_EN_15_16X9_TUBK1078000H_P00103751_OLV_HD"}],
+	"expand":{
+		"fullscreen":false,
+		"width":1280,
+		"height":418,
+		"indentAcross":0,
+		"indentDown":0
+  }
+});
---
+++ RM_No_Fees/Bank_HOLIDAY_COBG_No_Fees_NA_RM_RM_EN_NA_970x66_P00114026/manifest.js
@@ -0,0 +1,14 @@
+FT.manifest({
+	"filename":"Bank_Cafe_Spot_Check_NA_RM_RM_EN_NA_970x66_P00102777.html",
+	"width":970,
+	"height":66,
+	"clickTagCount":1,
+  "videos":[{"name":"video1", "ref":"202681/BANK_CAFE_SPOT_CHECK_NA_VID_VID_EN_15_16X9_TUBK1078000H_P00103751_OLV_HD"}],
+	"expand":{
+		"fullscreen":false,
+		"width":970,
+		"height":250,
+		"indentAcross":0,
+		"indentDown":0
+  }
+});
