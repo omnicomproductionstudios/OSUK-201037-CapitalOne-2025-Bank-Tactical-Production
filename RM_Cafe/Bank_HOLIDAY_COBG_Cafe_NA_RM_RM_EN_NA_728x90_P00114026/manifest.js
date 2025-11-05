e95f1aea647253966dbd0c440ca26537601c3b35
Mahesh Bhatt <141926871+adamandeveddbstudios@users.noreply.github.com>
2025-11-05 21:06:50 +0530

    cfgd

---
+++ RM_Cafe/Bank_HOLIDAY_COBG_Cafe_NA_RM_RM_EN_NA_728x90_P00114026/manifest.js
@@ -0,0 +1,14 @@
+FT.manifest({
+	"filename":"Bank_HOLIDAY_COBG_Cafe_NA_RM_RM_EN_NA_728x90_P00114026.html",
+	"width":728,
+	"height":90,
+	"clickTagCount":1,
+  "videos":[{"name":"video1", "ref":"202681/BANK_CAFE_SPOT_CHECK_NA_VID_VID_EN_15_16X9_TUBK1078000H_P00103751_OLV_HD"}],
+	"expand":{
+		"fullscreen":false,
+		"width":728,
+		"height":250,
+		"indentAcross":0,
+		"indentDown":0
+  }
+});
